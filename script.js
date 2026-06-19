7c3405233234508a8dfa1e531a761550eb7dffc6
Darkeeez12 <alifghaz12@gmail.com>
2026-06-19 16:18:14 +0700

    Done

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const cursor = document.querySelector('.cursor');
-    setInterval(() => {
-        cursor.style.opacity = cursor.style.opacity === '0' ? '1' : '0';
-    }, 800);
 
     const bootTextLines = [
         "ROBCO INDUSTRIES (TM) UNIFIED OPERATING SYSTEM", "COPYRIGHT 2075-2077 ROBCO INDUSTRIES", " ", "-SERVER 1-",
@@ -54,16 +50,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
     const navLinks = document.querySelectorAll('.nav-link');
     const sections = document.querySelectorAll('.content-section');
-    navLinks.forEach(link => {
-        link.addEventListener('click', function(event) {
-            event.preventDefault();
-            navLinks.forEach(nav => nav.classList.remove('active'));
-            sections.forEach(sec => sec.classList.remove('active'));
-            this.classList.add('active');
-            const targetId = this.getAttribute('href');
-            document.querySelector(targetId).classList.add('active');
-        });
-    });
 
     function playTerminalSound() {
         const audio = new Audio('Assets/PipBoy_Select.mp3');
